a97c86d117d2da443e1076dfae97f88009ef7e8d
pkorchak <paulkorchak@gmail.com>
2020-01-03 00:09:52 +0300

    Added dynamic flexbox columns grid; button and input stylization

--- src/app/pages/pre-built-forms/register/register-form-view.component.ts
+++ src/app/pages/pre-built-forms/register/register-form-view.component.ts
@@ -8,37 +8,42 @@ import { FormElementType, FormStructure } from '@widgets/form-viewer';
 export class RegisterFormViewComponent {
 
   formStructure: FormStructure = {
-    columnsNum: 1,
+    columnsNum: 2,
     name: 'Create an account',
     elements: [
       {
         label: 'Email',
         name: 'fc-1',
         type: FormElementType.INPUT,
-        required: true
+        required: true,
+        placeholder: 'Enter email'
       },
       {
         label: 'First Name',
         name: 'fc-2',
         type: FormElementType.INPUT,
-        required: true
+        required: true,
+        placeholder: 'Enter first name'
       },
       {
         label: 'Last Name',
         name: 'fc-3',
-        type: FormElementType.INPUT
+        type: FormElementType.INPUT,
+        placeholder: 'Enter last name'
       },
       {
         label: 'Password',
         name: 'fc-4',
         type: FormElementType.INPUT,
-        required: true
+        required: true,
+        placeholder: 'Enter password'
       },
       {
         label: 'Repeat Password',
         name: 'fc-5',
         type: FormElementType.INPUT,
-        required: true
+        required: true,
+        placeholder: 'Repeat password'
       }
     ]
   };
--- src/app/widgets/form-viewer/form-viewer.module.ts
+++ src/app/widgets/form-viewer/form-viewer.module.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FormViewerComponent } from './form-viewer.component';
 import { FormViewerElementComponent } from './element/form-viewer-element.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NzButtonModule, NzInputModule } from 'ng-zorro-antd';
 
 @NgModule({
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NzInputModule,
+    NzButtonModule
   ],
   declarations: [FormViewerComponent, FormViewerElementComponent],
   exports: [FormViewerComponent]

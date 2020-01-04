780e3859d1fdb578224252de52fd04cab5171ec9
pkorchak <paulkorchak@gmail.com>
2020-01-04 11:36:56 +0300

    Import register form structure as a separate json file

--- src/app/pages/pre-built-forms/register/register-form-view.component.ts
+++ src/app/pages/pre-built-forms/register/register-form-view.component.ts
@@ -1,50 +1,11 @@
 import { Component } from '@angular/core';
-import { FormElementType, FormStructure } from '@widgets/form-viewer';
+import { FormStructure } from '@widgets/form-viewer';
+import registerFormStructure from './register-form-structure.json';
 
 @Component({
   selector: 'app-register-form-view',
   template: '<app-form-viewer [formStructure]="formStructure"></app-form-viewer>'
 })
 export class RegisterFormViewComponent {
-
-  formStructure: FormStructure = {
-    columnsNum: 2,
-    name: 'Create an account',
-    elements: [
-      {
-        label: 'Email',
-        name: 'fc-1',
-        type: FormElementType.INPUT,
-        required: true,
-        placeholder: 'Enter email'
-      },
-      {
-        label: 'First Name',
-        name: 'fc-2',
-        type: FormElementType.INPUT,
-        required: true,
-        placeholder: 'Enter first name'
-      },
-      {
-        label: 'Last Name',
-        name: 'fc-3',
-        type: FormElementType.INPUT,
-        placeholder: 'Enter last name'
-      },
-      {
-        label: 'Password',
-        name: 'fc-4',
-        type: FormElementType.INPUT,
-        required: true,
-        placeholder: 'Enter password'
-      },
-      {
-        label: 'Repeat Password',
-        name: 'fc-5',
-        type: FormElementType.INPUT,
-        required: true,
-        placeholder: 'Repeat password'
-      }
-    ]
-  };
+  formStructure: FormStructure = registerFormStructure;
 }

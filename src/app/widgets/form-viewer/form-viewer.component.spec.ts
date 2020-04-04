f9c0431616bded920c0889c39ad16f0957ec1228
pkorchak <paulkorchak@gmail.com>
2020-04-04 16:01:33 +0300

    test(form viewer): add FormViewerComponent tests

--- src/app/widgets/form-viewer/form-viewer.component.spec.ts
+++ src/app/widgets/form-viewer/form-viewer.component.spec.ts
@@ -1,25 +1,86 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FormViewerComponent } from './form-viewer.component';
+import { NzButtonModule } from 'ng-zorro-antd';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MockComponents } from 'ng-mocks';
+import { FormElementComponent } from '@widgets/form-viewer/element/form-element.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { FormElementType } from '@model/form-element-type';
+import { FormElementsFactory } from '@services/form-elements-factory';
+import { AbstractFormElement } from '@model/form-elements/abstract-form-element';
 
 describe('FormViewerComponent', () => {
   let component: FormViewerComponent;
   let fixture: ComponentFixture<FormViewerComponent>;
+  let root: DebugElement;
+  let formElementsFactory: FormElementsFactory;
+  let elements: AbstractFormElement[];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FormViewerComponent ]
-    })
-    .compileComponents();
+      declarations: [
+        FormViewerComponent,
+        MockComponents(FormElementComponent)
+      ],
+      imports: [
+        ReactiveFormsModule,
+        NzButtonModule
+      ]
+    }).compileComponents();
+
+    formElementsFactory = TestBed.inject(FormElementsFactory);
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(FormViewerComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    root = fixture.debugElement;
+
+    elements = [
+      formElementsFactory.createInstance({
+        label: 'Input',
+        type: FormElementType.SHORT_TEXT
+      }),
+      formElementsFactory.createInstance({
+        label: 'Checkbox',
+        type: FormElementType.CHECKBOX
+      })
+    ];
+    component.formStructure = {
+      name: 'Form name',
+      columnsNum: 1,
+      elements
+    };
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should show form name', () => {
+    fixture.detectChanges();
+    expect(root.query(By.css('.form-name')).nativeElement.innerText).toBe('Form name');
   });
+
+  it('should pass all elements to FormElementComponent instances', () => {
+    fixture.detectChanges();
+
+    const formElements = root.queryAll(By.directive(FormElementComponent));
+    expect(formElements.length).toBe(2);
+    expect(formElements[0].componentInstance.element).toBe(elements[0]);
+    expect(formElements[1].componentInstance.element).toBe(elements[1]);
+  });
+
+  [
+    {columnsNum: 1, columnWidth: '100%'},
+    {columnsNum: 2, columnWidth: '50%'},
+    {columnsNum: 3, columnWidth: '33.3333%'},
+    {columnsNum: 4, columnWidth: '25%'},
+  ].forEach(testData =>
+    it('column width should be ' + testData.columnWidth + ' for columnsNum ' + testData.columnsNum, () => {
+      component.formStructure.columnsNum = testData.columnsNum;
+      component.ngOnChanges();
+      fixture.detectChanges();
+
+      root.queryAll(By.css('.control-container'))
+        .forEach(controlContainer => expect(controlContainer.styles.width).toBe(testData.columnWidth));
+    }));
 });

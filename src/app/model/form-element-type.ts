9b52b2a8a3dfd7afdc74b57f23a663ec430bbefd
Pavel Korchak <paulkorchak@gmail.com>
2023-05-23 20:40:06 +0400

    refactor: unify icons naming

--- src/app/model/form-element-type.ts
+++ src/app/model/form-element-type.ts
@@ -1,6 +1,10 @@
+/**
+ * Form element control types.
+ * Correspond to the icon names from /assets/outline/ folder.
+ */
 export enum FormElementType {
-  SHORT_TEXT = 'shortText',
-  LONG_TEXT = 'longText',
+  SHORT_TEXT = 'short-text',
+  LONG_TEXT = 'long-text',
   CHECKBOX = 'checkbox',
   DATE = 'date',
   EMAIL = 'email',

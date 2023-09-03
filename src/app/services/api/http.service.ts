f1decaba853c7028d62a703d392f6c785df9542e
Pavel Korchak <paulkorchak@gmail.com>
2023-09-03 15:13:46 +0400

    feat: Call /users/register endpoint after SocialAuthService login

---
+++ src/app/services/api/http.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { catchError } from 'rxjs/operators';
+
+@Injectable({providedIn: 'root'})
+export class HttpService {
+
+  private headers: HttpHeaders = new HttpHeaders({'Content-Type': 'application/json'});
+
+  constructor(private http: HttpClient,
+              private router: Router) {
+  }
+
+  public post(url: string, body: unknown, params?: HttpParams): Observable<unknown> {
+    return this.handleResponse(this.http.post(url, body, {headers: this.headers, ...params}));
+  }
+
+  public get(url: string, params?: HttpParams): Observable<unknown> {
+    return this.handleResponse(this.http.get(url, {headers: this.headers, ...params}));
+  }
+
+  public patch(url: string, body: unknown, params?: HttpParams): Observable<unknown> {
+    return this.handleResponse(this.http.patch(url, body, {headers: this.headers, ...params}));
+  }
+
+  private handleResponse(response: Observable<unknown>): Observable<unknown> {
+    return response.pipe(catchError(err => this.catchError(err)));
+  }
+
+  private catchError(error: HttpErrorResponse): Observable<void> {
+    if (error.status === 401) {
+      this.router.navigateByUrl('/login');
+    }
+    // TODO Show notification with error text for the user
+    throw new Error(`${error.statusText}\n${error.message}`);
+  }
+}
